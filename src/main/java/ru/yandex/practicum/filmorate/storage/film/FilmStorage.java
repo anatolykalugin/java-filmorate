1556c03cdb0ef82a8bfc79228ee0d4c6972ac215
Anatoly <anatoly.kalugin4@yandex.ru>
2022-07-21 21:41:21 +0300

    fix: убрать дублирование методов в интерфейсах UserStorage и FilmStorage

--- src/main/java/ru/yandex/practicum/filmorate/storage/film/FilmStorage.java
+++ src/main/java/ru/yandex/practicum/filmorate/storage/film/FilmStorage.java
@@ -3,12 +3,6 @@ package ru.yandex.practicum.filmorate.storage.film;
 import ru.yandex.practicum.filmorate.model.Film;
 import ru.yandex.practicum.filmorate.storage.Storage;
 
-import java.util.List;
-
 public interface FilmStorage extends Storage<Film> {
-    Film getById(long id);
-    List<Film> showAll();
-    Film addNew(Film film);
-    Film update(Film film);
-    void deleteById(long id);
+
 }
--- src/main/java/ru/yandex/practicum/filmorate/storage/user/UserStorage.java
+++ src/main/java/ru/yandex/practicum/filmorate/storage/user/UserStorage.java
@@ -3,12 +3,6 @@ package ru.yandex.practicum.filmorate.storage.user;
 import ru.yandex.practicum.filmorate.model.User;
 import ru.yandex.practicum.filmorate.storage.Storage;
 
-import java.util.List;
-
 public interface UserStorage extends Storage<User> {
-    List<User> showAll();
-    User addNew(User user);
-    User update(User user);
-    User getById(long id);
-    void deleteById(long id);
+
 }
